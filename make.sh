e7d11d875832c04754916be9a56cce77b8aa83f9
Renan Iglesias <renan.iglesias@ufrj.br>
2015-02-28 19:03:42 -0300

    Make updated

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-javac src/br/ufrj/ppgi/huffmanyarnmultithreadv2/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/encoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/decoder/*.java -d bin
+javac src/br/ufrj/ppgi/huffmanyarnmultithreadv2/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/encoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/encoder/yarn/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/decoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/decoder/yarn/*.java -d bin
 
 ant
