581d81a2ccfe0b1794d84977c65625d010b3430b
Renan Iglesias <renan.iglesias@ufrj.br>
2015-03-22 22:27:31 -0300

    First commit

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p bin
+
 javac src/br/ufrj/ppgi/huffmanyarnmultithreadv2/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/encoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/encoder/yarn/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/decoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/decoder/yarn/*.java src/br/ufrj/ppgi/huffmanyarnmultithreadv2/tdd/*.java -d bin
 
 ant
